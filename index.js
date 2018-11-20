9c84161acc8f6f7723fce05900104dabb53972a0
William Robert Simpson <will.simpson85@gmail.com>
2018-11-20 17:55:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,25 +8,19 @@ function setCart(c) {
   cart = c;
   return cart;
 }
-
+var price = Math.floor(Math.random() * 101);
 function addToCart(item) {
  // write your code here
- var price = Math.floor(Math.random() * 101);
- cart.itemName = item;
- cart.itemPrice = price;
- var output = `${cart.itemName} has been added to your cart.`;
-return output
+ var item {};
+ item.itemPrice = price;
+ item[itemName] = item;
+ return `${item.itemName} has been added to your cart.`
 }
 
-function viewCart(){
-  //Should loop over every item in the cart
-  //return a statement of what items are in the cart
-  if (cart.itemName.length <= 0)
-  {
-    return "Your shopping cart is empty.";
-  } else {
-    return null};
+function viewCart() {
+  // write your code here
 }
+
 function total() {
   // write your code here
 }
