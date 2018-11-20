1816cc67b3191605f851fe74b545e8dd66c3fb1c
William Robert Simpson <will.simpson85@gmail.com>
2018-11-20 19:08:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function setCart(c) {
 
 function addToCart(item) {
  // write your code here
- newItem.price = random;
+ newItem.price = parseInt(random);
  newItem.itemName = item;
  cart.push(newItem);
 return `${newItem.itemName} has been added to your cart.`
