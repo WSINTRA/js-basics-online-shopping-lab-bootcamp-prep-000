14728fb7eecb4c5a6e497b828637b4a94dfe92f6
William Robert Simpson <will.simpson85@gmail.com>
2018-12-06 21:25:53 +0000

    Automatically backed up by Learn

--- testing.js
+++ testing.js
@@ -1,3 +1,49 @@
+var cartItem = {}
+var price = Math.floor(Math.random() * 101);
+function addToCart(item) {
+ // write your code here
+
+
+ cartItem.itemName = item;
+ cartItem.itemPrice = price;
+
+ var output = `${cartItem.itemName} has been added to your cart. Item price is ${cartItem.itemPrice}`;
+
+
+
+return output
+}
+const fruits = {
+  apple: 28,
+  orange: 17,
+  pear: 54,
+}
+
+const entries = Object.entries(fruits)
+console.log(entries)
+for (const [fruit, count] of entries) {
+  console.log(`There are ${count} ${fruit}s`)
+}
+
+// Result
+// There are 28 apples
+// There are 17 oranges
+// There are 54 pears
+console.log(addToCart(""))
+function viewCart(){
+  //Should loop over every item in the cart
+  //return a statement of what items are in the cart
+  if (cartItem.itemName.length <= 0)
+  {
+    return "Your shopping cart is empty.";
+  } else {
+    return null};
+}
+console.log(viewCart())
+console.log(cartItem.itemName.length)
+
+
+
 var cart = [];
 
 
@@ -73,8 +119,4 @@ function removeFromCart(item) {
 Check each object's itemName value key to see if it matches the parameter, then remove it if it matches.
 You might find [Array.prototype.splice()](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/splice) to be useful.
 + If the cart does not contain a matching item, the function should return `That item is not in your cart.`*/
-}
-
-function placeOrder(cardNumber) {
-  // write your code here
 }
